8b103f7593ba73fc42394f5d1002a804615a0dfd
Brian Adams <service@contrasting.org>
2022-09-14 15:01:14 -0500

    Remove unnecessary iam info

--- modules/gh-oidc/main.tf
+++ modules/gh-oidc/main.tf
@@ -41,7 +41,7 @@ resource "google_iam_workload_identity_pool_provider" "main" {
 resource "google_service_account_iam_member" "wif-sa" {
   for_each           = var.sa_mapping
   service_account_id = each.value.sa_name
-  role               = "roles/owner"
+  role               = "roles/iam.workloadIdentityUser"
   member             = "principalSet://iam.googleapis.com/projects/1037290380438/locations/global/workloadIdentityPools/terrateam-tf/*"
 }
 
